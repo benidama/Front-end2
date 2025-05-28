b0bb0d040c39d01d679a3b22fced1f1f201e43ba
benidama <benidama02@gmail.com>
2025-05-28 15:20:13 +0000

    paragraph added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -22,6 +22,7 @@ const Home = () => {
   return (
     <div className="home">
       <h1>All Posts from backend data</h1>
+      <p>Lorem is the biggest in the continent especially in the Africa.</p>
       <div className="posts">
         {posts?.map((post) => (
           <PostDetails post={post} key={post._id} />
