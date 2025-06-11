dd6f3e62f8f7478cd0ed758aa8308b41c7522943
benidama <benidama02@gmail.com>
2025-06-11 11:27:29 +0000

    Do it

--- src/components/Push.jsx
+++ src/components/Push.jsx
@@ -2,7 +2,10 @@ const Push = () => {
   return (
     <div>
       <h1>Hello my love, I really miss you very much!</h1>
-      <p>Lorem ipsum dolor sit amet.</p>
+      <p>
+        Lorem ipsum dolor sit amet. Laurent is the best one in this world, so we
+        can suport him in all sides
+      </p>
     </div>
   );
 };
