006eb0f3457c5a7d913d7cc2cc8f42599e21cc01
benidama <benidama02@gmail.com>
2025-06-17 17:44:49 +0000

    changes added

--- src/components/Push.jsx
+++ src/components/Push.jsx
@@ -3,7 +3,9 @@ const Push = () => {
     <div>
       <h1>Hello my love, I really miss you very much!</h1>
       <p>
-        Lorem ipsum dolor sit amet.
+        Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consectetur
+        adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore
+        magna aliqua. Ut enim ad minim veniam.
       </p>
     </div>
   );
