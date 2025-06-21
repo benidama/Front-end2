e8d3a186852f7b25c090e52c3f31cbbae93db740
benidama <benidama02@gmail.com>
2025-06-21 21:01:40 +0000

    Paragraph added

--- src/components/Push.jsx
+++ src/components/Push.jsx
@@ -4,7 +4,8 @@ const Push = () => {
       <h1>Hello my love, I really miss you very much!</h1>
       <p>
         Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consectetur
-        adipiscing elit. How can I do this myself?
+        adipiscing elit. How can I do this myself? But i will do everything in
+        order to know all necessaries.
       </p>
       <div>
         <h3>I want to express my deepest feeling about you my love</h3>
