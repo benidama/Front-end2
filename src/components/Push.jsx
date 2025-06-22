fb073334fed9ca891518a48e58e06fc4cc26b5f9
benidama <benidama02@gmail.com>
2025-06-22 09:11:12 +0000

    fix error

--- src/components/Push.jsx
+++ src/components/Push.jsx
@@ -3,9 +3,8 @@ const Push = () => {
     <div>
       <h1>Hello my love, I really miss you very much!</h1>
       <p>
-        Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consectetur
-        adipiscing elit. How can I do this myself? But i will do everything in
-        order to know all necessaries.
+        Lorem ipsum dolor sit amet. How can I do this myself? But i will do
+        everything in order to know all necessaries.
       </p>
       <div>
         <h3>I want to express my deepest feeling about you my love</h3>
