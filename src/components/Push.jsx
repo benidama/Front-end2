56688117dec1499508919ca61734b63413cda0f1
benidama <benidama02@gmail.com>
2025-06-23 19:25:09 +0000

    para added

--- src/components/Push.jsx
+++ src/components/Push.jsx
@@ -3,8 +3,10 @@ const Push = () => {
     <div>
       <h1>Hello my love, I really miss you very much!</h1>
       <p>
-        Lorem ipsum dolor sit amet. How can I do this myself? But i will do
-        everything in order to know all necessaries.
+        How can I do this myself? But I will do everything in order to know all
+        necessaries. I will do everything to be with you, my love. I will always
+        be with you, no matter what. I will always support you and be there for
+        you. You are my everything, and I will never let you go.
       </p>
       <div>
         <h3>I want to express my deepest feeling about you my love</h3>
