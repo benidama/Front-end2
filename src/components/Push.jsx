b216198c81557da84b36cf370acfba73ee9187c5
Benimana Jean Damascene <benidama02@gmail.com>
2025-06-25 20:06:21 -0700

    all changes added

--- src/components/Push.jsx
+++ src/components/Push.jsx
@@ -4,7 +4,7 @@ const Push = () => {
       <h1>Hello my love, I really miss you very much!</h1>
       <p>
         How can I do this myself? But I will do everything in order to know all
-        necessaries. I will do everything to be with you, my love. I will always
+        necessaries. I will always
         be with you, no matter what. I will always support you and be there for
         you. You are my everything, and I will never let you go.
       </p>
