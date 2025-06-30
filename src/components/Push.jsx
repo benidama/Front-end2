93741108a5623826494514ab5d74615e7708a3f8
Benimana Jean Damascene <benidama02@gmail.com>
2025-06-30 13:38:51 -0700

    remove some errors

--- src/components/Push.jsx
+++ src/components/Push.jsx
@@ -2,14 +2,14 @@ const Push = () => {
   return (
     <div>
       <h1>Hello my love, I really miss you very much!</h1>
-      <p class="para">
+      <p >
         How can I do this myself? But I will do everything in order to know all
         necessaries. I will always support you and be there for
-        you. You are my everything, and I will never let you go.
+        you. 
       </p>
       <div>
         <h3>I want to express my deepest feeling about you my love</h3>
-        <p>Hello my love, I really miss you very much!
+        <p className="para">Hello my love, I really miss you very much!
           Today, I learned all about AI and self express. Everything is okay for me and to know if you and others are in the page.
         </p>
       </div>
